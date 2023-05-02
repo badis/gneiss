fc9eafef397eefc44ffaae795cef5cf928ec21b0
Badis Merabet <badisinbox@gmail.com>
2023-05-02 03:46:42 +0100

    Setup material ui

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +17,11 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
             <a href="/about">About</a>
+            <Box component="form">
+              <Button variant="outlined"> 
+                Submit
+              </Button> 
+            </Box>
       </main>
     </>
   )
