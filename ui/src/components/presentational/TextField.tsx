b0d535d1a20849848faf801c6da607b3d03a635e
Badis Merabet <badis.merabet@getrocket.com>
2023-05-03 01:36:06 +0100

    Feature:Posts > UI > Develop post creation form
    Author:    Badis Merabet <badis.merabet@outlook.com>

--- ui/src/components/presentational/TextField.tsx
+++ ui/src/components/presentational/TextField.tsx
@@ -1,13 +1,47 @@
+import { Colors } from "@/theme";
 import { TextField } from "@mui/material";
 import { FC } from "react";
 
 interface StyledTextFieldProps {
   label: string;
   multiline?: boolean;
+  size?: "small" | "medium";
 }
 
-const StyledTextField: FC<StyledTextFieldProps> = ({ label, multiline }) => {
-  return <TextField label={label} multiline maxRows={8} />;
+const StyledTextField: FC<StyledTextFieldProps> = ({
+  label,
+  multiline,
+  size,
+}) => {
+  return (
+    <TextField
+      label={label}
+      multiline={multiline}
+      maxRows={8}
+      fullWidth={true}
+      size={size}
+      sx={{
+        "& label": {
+          fontSize: "default",
+        },
+        "& .MuiOutlinedInput-root": {
+          fontSize: "default",
+          "& fieldset": {
+            padding: "8px",
+            border: `2px solid ${Colors.grays[200]}`,
+            borderRadius: "4px",
+            boxShadow: "none",
+          },
+          "&:hover:not(&.Mui-focused) fieldset": {
+            borderColor: Colors.grays[200],
+          },
+          // "&.Mui-focused fieldset": {
+          //   borderColor: "#ededed",
+          // },
+        },
+      }}
+    />
+  );
 };
 
 export { StyledTextField as TextField };
