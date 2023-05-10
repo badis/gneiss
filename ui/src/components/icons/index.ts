57b7ca9c55621668fbc2125dbb6d514de16ef783
Badis Merabet <badisinbox@gmail.com>
2023-05-10 13:42:20 +0100

    Feature:Likes > UI > Fix like icon

--- ui/src/components/icons/index.ts
+++ ui/src/components/icons/index.ts
@@ -2,5 +2,6 @@ export {
   Delete as DeleteIcon,
   Edit as EditIcon,
   Favorite as FavoriteIcon,
+  FavoriteBorder as FavoriteBorderIcon,
   MoreVert as MoreVertIcon,
 } from "@mui/icons-material";
