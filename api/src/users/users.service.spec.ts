53beb49b1d6da39df8bbd7c5c0b828fdf12d1320
Badis Merabet <badisinbox@gmail.com>
2023-06-14 14:43:43 +0100

    Rest API > unit testing

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -3,11 +3,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { databaseProviders } from '@/database/database.providers';
 import { UsersService } from '@/users/users.service';
-
-import { userProviders } from './user.providers';
+import { User } from './entities';
 
 describe('UsersService', () => {
   let service: UsersService;
+
+  const userMock = {
+    username: 'username',
+    password: 'password',
+    email: 'email@email.com',
+  } as unknown as User;
+
+  const mockUserRepository = {
+    create: jest.fn().mockImplementation(dto => dto),
+    save: jest.fn().mockImplementation(user => {
+      const { password: _, ...rest } = user;
+      return Promise.resolve({ id: Date.now(), ...rest });
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,7 +29,14 @@ describe('UsersService', () => {
           isGlobal: true,
         }),
       ],
-      providers: [...databaseProviders, ...userProviders, UsersService],
+      providers: [
+        ...databaseProviders,
+        {
+          provide: 'USER_REPOSITORY',
+          useValue: mockUserRepository,
+        },
+        UsersService,
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -24,5 +44,14 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const user = await service.create(userMock);
+    expect(user).toEqual({
+      id: expect.any(Number),
+      email: userMock.email,
+      username: userMock.username,
+    });
   });
 });
